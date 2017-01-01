61e131015e5a8b40c2d547aa87d9836e523d1582
Samo Kralj <samokralj95@gmail.com>
2017-01-01 16:18:57 +0100

    Template url-i glede na course

--- web/tomo_statistics/urls.py
+++ web/tomo_statistics/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main_view, name='main_view'),
+    url(r'^(?P<course_pk>\d+)/$', views.test_view, name = 'main_view')
+    
 ]
--- web/tomo_statistics/views.py
+++ web/tomo_statistics/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from courses.models import Course, ProblemSet
 
 def main_view(request):
     return render(request, 'tomo_statistics/statistika_test.html')
+
+def test_view(request, course_pk):
+    course = get_object_or_404(Course, pk = course_pk)
+    students = course.student_success()
+    return render(request, 'tomo_statistics/statistika_course.html',
+                  {'students' : students})
