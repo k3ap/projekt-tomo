ee5ef15a7125b2c934790d7fa30468ecfa720d7b
Samo Kralj <samokralj95@gmail.com>
2017-08-22 08:29:19 +0200

    Popravljen template za čas oddaje, da se čas dejansko izpiše + lepotni popravek

--- web/web/settings/local.py
+++ web/web/settings/local.py
@@ -21,7 +21,7 @@ INTERNAL_IPS = ['127.0.0.1']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tomo',
+        'NAME': 'tomo2',
         'USER': 'tomo',
         'PASSWORD': 'tomo',
         'HOST': 'localhost',
