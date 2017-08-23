0ded3565f01e0a23d9ec85cdb485ea6155f3f3b0
Samo Kralj <samokralj95@gmail.com>
2017-08-23 17:37:47 +0200

    Popravljeni url naslovi in imena pogledov, počiščeni templatei

--- web/tomo_statistics/urls.py
+++ web/tomo_statistics/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_view, name='main_view'),
-    url(r'^(?P<course_pk>\d+)/$', views.test_view, name = 'test_view'),
-    url(r'^graf_test/(?P<course_pk>\d+)/$', views.graph, name='graph'),
-    url(r'^(?P<course_pk>\d+)/js$', views.graph_json, name='graph_json'),
+    url(r'^(?P<course_pk>\d+)/$', views.course_graphs, name = 'course_graphs'),
+    url(r'^(?P<course_pk>\d+)/users$', views.user_success, name='user_success'),
 ]
--- web/tomo_statistics/views.py
+++ web/tomo_statistics/views.py
@@ -4,21 +4,14 @@ from .models import Course
 from django.http import HttpResponse, JsonResponse
 import json
 
-def main_view(request):
-    return render(request, 'tomo_statistics/statistika_test.html')
-
-def test_view(request, course_pk):
+def course_graphs(request, course_pk):
     course = get_object_or_404(Course, pk=course_pk)
     return render(request, 'tomo_statistics/statistika_course.html', {
-        'annotated_problem_sets' : course.problem_success()
+        'annotated_problem_sets' : course.problem_success(),
+        'course' : course,
     })
 
-def graph(request, course_pk):
+def user_success(request, course_pk):
     course = get_object_or_404(Course, pk=course_pk)
     json_data = json.dumps(course.statistika()['solved_atleast'])
-    return render(request, 'tomo_statistics/graf.html', {'json_data' : json_data})
-
-def graph_json(request, course_pk):
-    course = get_object_or_404(Course, pk=course_pk)
-    response = JsonResponse(course.json_za_graf(), safe = False)
-    return response
+    return render(request, 'tomo_statistics/user_success.html', {'json_data' : json_data})
